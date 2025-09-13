2658eead9da8bc17dd1f8358d33db94b772445e5
Sara Ammourh <saraammourh814@gmail.com>
2025-09-13 18:32:03 +0300

    Implemented concrete observer User

--- src/main/java/org/example/observer/User.java
+++ src/main/java/org/example/observer/User.java
@@ -9,7 +9,7 @@ public class User implements Observer {
 
     @Override
     public void update(String message) {
-        // TODO: Print a notification message for this user
+        System.out.println("Notification for " + username + ": " + message);
     }
 
     public String getUsername() {

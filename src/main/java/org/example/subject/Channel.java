adf21d5ea1ce4a215c1bcaff3f96f3c61bb05174
Sara Ammourh <saraammourh814@gmail.com>
2025-09-13 18:28:03 +0300

    Implemented concrete class Channek

--- src/main/java/org/example/subject/Channel.java
+++ src/main/java/org/example/subject/Channel.java
@@ -2,30 +2,40 @@ package org.example.subject;
 
 import org.example.observer.Observer;
 
+import java.util.ArrayList;
+
+import java.util.List;
+
+
 public class Channel implements Subject{
     private final String name;
+
+    private final List<Observer> subscribers = new ArrayList<>();
 
     public Channel(String name) {
         this.name = name;
     }
 
     public void uploadVideo(String title){
-        // TODO: Notify all subscribers about new video
+        String message = this.name + " uploaded a new video: " + title;
+        this.notifyObservers(message);
     }
 
     @Override
     public void subscribe(Observer observer) {
-        // TODO: Add subscriber
+        this.subscribers.add(observer);
     }
 
     @Override
     public void unsubscribe(Observer observer) {
-        // TODO: Remove subscriber
+        this.subscribers.remove(observer);
     }
 
     @Override
     public void notifyObservers(String message) {
-        // TODO: Notify subscribers
+        for(Observer observer : this.subscribers){
+            observer.update(message);
+        }
     }
 
     public String getName() {
